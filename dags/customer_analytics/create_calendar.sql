7b693d9c059daa439be6ed3ba3066b4dc48150aa
Bob Briski <bbriski@raybeam.com>
2020-04-06 17:19:19 -0700

    ok, really now

--- dags/customer_analytics/create_calendar.sql
+++ dags/customer_analytics/create_calendar.sql
@@ -1,3 +1,3 @@
-create table {{ var.value.database }}.public.calendar as
+create or replace table {{ var.value.database }}.public.calendar as
 select *
 from {{ var.value.sample_database }}.tpcds_sf10tcl.date_dim;
